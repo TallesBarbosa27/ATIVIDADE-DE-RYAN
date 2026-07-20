c7458ee94f73a7cb3cd01631ada909b65162ebc9
TallesBarbosa27 <tallesbarbosaoliveira@gmail.com>
2026-07-20 17:13:02 -0300

    Create Dados.js to manage book data
    dados

---
+++ Dados.js
@@ -0,0 +1,9 @@
+// dados.js
+
+// Array de objetos que armazenará os livros
+const bancoDeDados = {
+    livros: []
+};
+
+// Exportamos o objeto para que outros arquivos possam acessá-lo e modificá-lo
+module.exports = bancoDeDados;
